ace8d75b39d95604e8e50fe571adfef0ba13e201
Jack Kingsman <jack.kingsman@gmail.com>
2015-11-08 19:02:07 -0800

    add ios short circuit for in app

--- src/lib/init.js
+++ src/lib/init.js
@@ -1,7 +1,15 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav({closeOnClick: true});
-
+(function($) {
+  $(function() {
+    $('.button-collapse').sideNav({
+      closeOnClick: true
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).on('click', 'a', function(event) {
+  if ($(event.target).attr('class') == undefined || $(event.target).attr('class').indexOf('page-action') === -1) {
+    console.log('preving')
+    event.preventDefault();
+    window.location = $(this).attr("href");
+  }
+});
