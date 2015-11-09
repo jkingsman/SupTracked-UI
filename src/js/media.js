04d3e22a0965d0c148d1cd09d17cf490faab9acf
Jack Kingsman <jack.kingsman@gmail.com>
2015-11-09 15:45:43 -0800

    add media finished

--- src/js/media.js
+++ src/js/media.js
@@ -1,4 +1,4 @@
-/* globals makeAuthRequest,Materialize,getCookie,makeAuthBlobRequest */
+/* globals makeAuthRequest,Materialize,getCookie,makeAuthBlobRequest,server */
 /* jshint -W089 */
 
 "use strict";
@@ -17,7 +17,6 @@ makeAuthRequest('/media/search', 'POST', null, 'json', function(err, data, code)
 
 function loadMore() {
   if (!atEnd) {
-    atEnd = true;
     makeAuthRequest('/media/search', 'POST', JSON.stringify({
       limit: batchSize,
       offset: currentBatch * batchSize
@@ -39,7 +38,7 @@ function loadMore() {
         var mediaUrl = getCookie('server') + '/media/file/' + media.id;
 
         var association = '';
-        if(media.association_type === 'experience'){
+        if (media.association_type === 'experience') {
           association = '<br><a href="/experience.html?' + media.association + '">View Experience</a>';
         }
 
@@ -61,7 +60,98 @@ function loadMore() {
   }
 }
 
-loadMore();
+function prepareAdd() {
+  var today = new Date();
+  var dateString = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2) + ' ' + ('0' + today.getHours()).slice(-2) + ('0' + today.getMinutes()).slice(-2);
+  $('#customTime').val(dateString);
+  makeAuthRequest('/experience/search', 'POST', null, 'json', function(err, data, code) {
+    if (data.length < 1) {
+      $('#experience').append('<option value="" disabled selected>None</option>');
+      return;
+    }
+
+    data.sort(function(a, b) {
+      return parseFloat(b.date) - parseFloat(a.date);
+    });
+
+    data.forEach(function(experience) {
+      $('#experience').append('<option value="' + experience.id + '">' + new Date(experience.date * 1000).toISOString().slice(0, 10) + ' -- ' + experience.title + '</option>');
+    });
+  });
+}
+
+$('#addMedia').submit(function(event) {
+  event.preventDefault();
+
+  // assemble this horrible date
+  var customDate = $('#customTime').val().split(' ')[0];
+  var customTime = $('#customTime').val().split(' ')[1];
+  var customDateStamp = Math.floor(new Date(customDate).getTime() / 1000);
+
+  // add hours and minutes
+  customDateStamp += Math.floor(customTime / 100) * 3600;
+  customDateStamp += (customTime - (Math.floor(customTime / 100) * 100)) * 60;
+
+  // build the form
+  var formData = new FormData();
+  formData.append("title", $('#title').val());
+  formData.append("tags", $('#tags').val());
+
+  if ($('input:radio[name=time]:checked').val() === 'custom') {
+    formData.append("date", customDateStamp);
+  }
+
+  formData.append("association_type", 'experience');
+  formData.append("association", $('#experience').val());
+
+  if ($('#favorite').is(':checked')) {
+    formData.append("favorite", 1);
+  }
+
+  if ($('#explicit').is(':checked')) {
+    formData.append("explicit", 1);
+  }
+
+  formData.append("image", $('#mediaFile').prop('files')[0]);
+
+  var auth = getCookie('auth');
+  var server = getCookie('server');
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.onload = function(e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 201) {
+        $("#addMediaModal").closeModal();
+        Materialize.toast('Media added', 6000, 'success-toast');
+        setTimeout(function(){
+          window.location = '/media.html';
+        }, 1000);
+      } else {
+        $("#addMediaModal").closeModal();
+        Materialize.toast(xhr.statusText, 4000, 'warning-toast');
+      }
+    }
+  };
+
+  xhr.onerror = function(e) {
+    $("#addMediaModal").closeModal();
+    Materialize.toast(xhr.statusText, 4000, 'warning-toast');
+  };
+
+  xhr.open("POST", server + '/media');
+  xhr.setRequestHeader('Authorization', 'Basic ' + auth);
+  xhr.send(formData);
+});
+
+// handle custom time show/hide
+$('input:radio[name=time]').on('change', function() {
+  if ($('input:radio[name=time]:checked').val() === 'custom') {
+    $('#customTimeArea').show();
+  } else {
+    $('#customTimeArea').hide();
+  }
+});
 
 $(window).scroll(function() {
   if ($(window).scrollTop() + $(window).height() > $(document).height() - 50) {
@@ -69,6 +159,5 @@ $(window).scroll(function() {
   }
 });
 
-function newMedia(){
-  $('#addMediaModal').openModal();
-}
+loadMore();
+prepareAdd();
