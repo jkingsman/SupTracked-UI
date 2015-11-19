5cc12a6870b658aed9eb50ec9977e1baba7fc42b
Jack Kingsman <jack.kingsman@gmail.com>
2015-11-18 16:58:11 -0800

    add ssl stuff

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var serveStatic = require('serve-static');
 var compression = require('compression');
+var https = require('https');
+var fs = require('fs');
 
 var app = express();
 
 app.use(compression());
 app.use(serveStatic(__dirname + '/dist'));
-app.listen(8080);
+
+
+https.createServer({
+  key: fs.readFileSync('ssl/key.key'),
+  cert: fs.readFileSync('ssl/cert.crt'),
+  ca: fs.readFileSync('ssl/bundle.ca-bundle')
+}, app).listen(8080);
 
 console.log('Listening on :8080');
