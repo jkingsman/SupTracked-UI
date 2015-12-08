39e0f0a1f84aeb73e809d4e025e7d33c70d87f7e
Jack Kingsman <jack.kingsman@gmail.com>
2015-12-07 23:29:19 -0800

    add no results msg, no rating search, and top scroll/filter collapse

--- src/js/experiences.js
+++ src/js/experiences.js
@@ -149,7 +149,7 @@ $('#filterForm').submit(function(event) {
     filterCriteria.notes = $('#filterNotes').val();
   }
 
-  if ($('#filterRating').val() > 0) {
+  if ($('#filterRating').val() > -1) {
     filterCriteria.rating_id = $('#filterRating').val();
   }
 
@@ -178,83 +178,92 @@ $('#filterForm').submit(function(event) {
     function(err, data, code) {
       $(".collection-item").remove(); // clear existing entries
       $(window).off("scroll", autoLoader); // stop listening for scroll; we're loading them all at once now
-      data.sort(function(a, b) {
-        return parseFloat(b.date) - parseFloat(a.date);
-      });
 
-      data.forEach(function(experience) {
-        // how we do a ghetto high speed search without parsing json
-        var conString = JSON.stringify(experience.consumptions).toLowerCase();
-        if (conString.indexOf($('#filterLocation').val().toLowerCase()) === -1 ||
-          conString.indexOf($('#filterFriends').val().toLowerCase()) === -1 ||
-          conString.indexOf($('#filterDrug').val().toLowerCase()) === -1 ||
-          conString.indexOf($('#filterMethod').val().toLowerCase()) === -1) {
-          // don't match our criteria
-          return;
-        }
+      // collapse the filter and go to top
+      $('.collapsible-header').click();
+      $(window).scrollTop(0);
 
-
-        if (experience.title.length < 1) {
-          experience.title = '[none]';
-        }
-
-        // compile thr consumptions, grouped by drug
-        var groupedConsumptionList = {};
-
-        experience.consumptions.forEach(function(consumption) {
-          if (groupedConsumptionList.hasOwnProperty(consumption.drug.name)) {
-            groupedConsumptionList[consumption.drug.name].count += consumption.count;
-          } else {
-            groupedConsumptionList[consumption.drug.name] = {};
-            groupedConsumptionList[consumption.drug.name].count = consumption.count;
-            groupedConsumptionList[consumption.drug.name].unit = consumption.drug.unit;
-          }
+      if (data) {
+        data.sort(function(a, b) {
+          return parseFloat(b.date) - parseFloat(a.date);
         });
 
-        // group the consumptions into strings by drug
-        var stringifiedConsumptions = [];
-
-        if (Object.keys(groupedConsumptionList).length > 0) {
-          for (var drug in groupedConsumptionList) {
-            stringifiedConsumptions.push(groupedConsumptionList[drug].count + ' ' + groupedConsumptionList[drug].unit + ' ' + drug);
+        data.forEach(function(experience) {
+          // how we do a ghetto high speed search without parsing json
+          var conString = JSON.stringify(experience.consumptions).toLowerCase();
+          if (conString.indexOf($('#filterLocation').val().toLowerCase()) === -1 ||
+            conString.indexOf($('#filterFriends').val().toLowerCase()) === -1 ||
+            conString.indexOf($('#filterDrug').val().toLowerCase()) === -1 ||
+            conString.indexOf($('#filterMethod').val().toLowerCase()) === -1) {
+            // don't match our criteria
+            return;
           }
-        } else {
-          stringifiedConsumptions.push('no consumptions');
-        }
 
-        // compile friends list
-        var groupedFriendsList = [];
-        var groupedFriendsString = 'Solo Experience';
 
-        experience.consumptions.forEach(function(consumption) {
-          consumption.friends.forEach(function(friend) {
-            if (groupedFriendsList.indexOf(friend.name) === -1) {
-              groupedFriendsList.push(friend.name);
+          if (experience.title.length < 1) {
+            experience.title = '[none]';
+          }
+
+          // compile thr consumptions, grouped by drug
+          var groupedConsumptionList = {};
+
+          experience.consumptions.forEach(function(consumption) {
+            if (groupedConsumptionList.hasOwnProperty(consumption.drug.name)) {
+              groupedConsumptionList[consumption.drug.name].count += consumption.count;
+            } else {
+              groupedConsumptionList[consumption.drug.name] = {};
+              groupedConsumptionList[consumption.drug.name].count = consumption.count;
+              groupedConsumptionList[consumption.drug.name].unit = consumption.drug.unit;
             }
           });
-        });
 
-        if (groupedFriendsList.length > 0) {
-          groupedFriendsString = groupedFriendsList.join(', ');
-        }
+          // group the consumptions into strings by drug
+          var stringifiedConsumptions = [];
 
-        // compile locations
-        var groupedLocationsList = [];
-        var groupedLocationsString = '[no location]';
-
-        experience.consumptions.forEach(function(consumption) {
-          if (groupedLocationsList.indexOf(consumption.location) === -1) {
-            groupedLocationsList.push(consumption.location);
+          if (Object.keys(groupedConsumptionList).length > 0) {
+            for (var drug in groupedConsumptionList) {
+              stringifiedConsumptions.push(groupedConsumptionList[drug].count + ' ' + groupedConsumptionList[drug].unit + ' ' + drug);
+            }
+          } else {
+            stringifiedConsumptions.push('no consumptions');
           }
+
+          // compile friends list
+          var groupedFriendsList = [];
+          var groupedFriendsString = 'Solo Experience';
+
+          experience.consumptions.forEach(function(consumption) {
+            consumption.friends.forEach(function(friend) {
+              if (groupedFriendsList.indexOf(friend.name) === -1) {
+                groupedFriendsList.push(friend.name);
+              }
+            });
+          });
+
+          if (groupedFriendsList.length > 0) {
+            groupedFriendsString = groupedFriendsList.join(', ');
+          }
+
+          // compile locations
+          var groupedLocationsList = [];
+          var groupedLocationsString = '[no location]';
+
+          experience.consumptions.forEach(function(consumption) {
+            if (groupedLocationsList.indexOf(consumption.location) === -1) {
+              groupedLocationsList.push(consumption.location);
+            }
+          });
+
+          if (groupedLocationsList.length > 0) {
+            groupedLocationsString = groupedLocationsList.join(', ');
+          }
+
+          $('#experiences-collection').append('<li class="collection-item">' + new Date(experience.date * 1000).toISOString().slice(0, 10) + '<span class="right hide-on-med-and-down" style="max-width: 50%;">' + groupedFriendsString + ' at <strong>' + groupedLocationsString + '</strong></span><h5><a href="/experience.html?' + experience.id + '">' + experience.title + '</a></h5><div class="pad-left-40">' + stringifiedConsumptions.join('<br />') + '</div></li>');
         });
-
-        if (groupedLocationsList.length > 0) {
-          groupedLocationsString = groupedLocationsList.join(', ');
-        }
-
-
-        $('#experiences-collection').append('<li class="collection-item">' + new Date(experience.date * 1000).toISOString().slice(0, 10) + '<span class="right hide-on-med-and-down" style="max-width: 50%;">' + groupedFriendsString + ' at <strong>' + groupedLocationsString + '</strong></span><h5><a href="/experience.html?' + experience.id + '">' + experience.title + '</a></h5><div class="pad-left-40">' + stringifiedConsumptions.join('<br />') + '</div></li>');
-      });
+      } else {
+        // no records
+        $('#experiences-collection').append('<li class="collection-item"><h5>No results</h5></li>');
+      }
     });
 });
 
