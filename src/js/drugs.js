89c56cad6e12523414fbdb4fe16123e3749fbec1
Jack Kingsman <jack.kingsman@gmail.com>
2015-12-07 21:29:36 -0800

    update markdown parsing with br stripping

--- src/js/drugs.js
+++ src/js/drugs.js
@@ -85,6 +85,16 @@ $("#drugList").on('change', function() {
       $('#notes').trigger('autoresize');
 
       var breaksToBR = micromarkdown.parse($('#notes').val()).replace(/(?:\r\n|\r|\n)/g, ' <br /> ');
+
+      // scrub out unnecessary breaks
+      breaksToBR = breaksToBR.replace(/(<br>\s*)+<br>/gm, '<br>');
+
+      //scrub out breaks before and after all h1-4
+      breaksToBR = breaksToBR.replace(/<\/h1>[\s<br />\s]+/g, '</h1>').replace(/[\s<br />\s]+<h1>/g, '<h1>');
+      breaksToBR = breaksToBR.replace(/<\/h2>[\s<br />\s]+/g, '</h2>').replace(/[\s<br />\s]+<h2>/g, '<h2>');
+      breaksToBR = breaksToBR.replace(/<\/h3>[\s<br />\s]+/g, '</h3>').replace(/[\s<br />\s]+<h3>/g, '<h3>');
+      breaksToBR = breaksToBR.replace(/<\/h4>[\s<br />\s]+/g, '</h4>').replace(/[\s<br />\s]+<h4>/g, '<h4>');
+
       $('#notesMarkdown').html(breaksToBR);
       $('#notes').hide();
       $('#notesMarkdown').show();
