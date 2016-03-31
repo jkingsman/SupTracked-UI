310f3beb8de5f0599328a51288e4cff3316d1099
Jack Kingsman <jack.kingsman@gmail.com>
2016-03-31 05:18:42 -0700

    Finish calendar

--- src/js/historical/calendar.js
+++ src/js/historical/calendar.js
@@ -1,7 +1,7 @@
 /* globals makeAuthRequest,Materialize */
 
 // these are predefined in the master historical.js file
-var analyticsCount, analyticsFinished, allDrugs, allConsumptions, allExperiences, dateToFormat, parseUrlParam;
+var analyticsCount, analyticsFinished, allDrugs, allConsumptions, allExperiences, dateToFormat, parseUrlParam, allDrugs;
 
 analyticsCount += 1;
 
@@ -14,18 +14,54 @@ function calendar() {
   endDay.setDate(endDay.getDate() + 1); // initial date jump
 
   // set it back and forward to the most recent sunday
-  if(currentDay.getDay() !== 0){
+  if (currentDay.getDay() !== 0) {
     currentDay.setDate(currentDay.getDate() - currentDay.getDay());
   }
 
-  if(endDay.getDay() !== 0){
+  if (endDay.getDay() !== 0) {
     endDay.setDate(endDay.getDate() + (6 - endDay.getDay()));
   }
 
-  while(currentDay <= endDay){
-    // console.log(dateToFormat(currentDay));
+  var column = 1;
+  var rowNumber = 0;
+  while (currentDay <= endDay) {
+    if (column === 7) {
+      column = 0;
+      rowNumber += 1;
+    }
 
+    if (column === 1) {
+      $('#calendar').append('<div class="row" id="calendarRow' + rowNumber + '"</div>');
+    }
+
+    $('#calendarRow' + rowNumber).append('<div class="col s12 m2 col' + column + '" style="min-height: 100px; border: 1px solid rgb(0, 0, 0);"><strong>' + dateToFormat(currentDay) + '</strong><hr>' +
+    '<ul style="margin-left: 15px; list-style-type: disc !important;" id="' + dateToFormat(currentDay) + '"></ul></div>');
+
+    column += 1;
     currentDay.setDate(currentDay.getDate() + 1);
+  }
+
+  // figure out drugs per day
+  var drugsPerDay = {};
+  allConsumptions.forEach(function(consumption) {
+    var conDate = dateToFormat(new Date(consumption.date * 1000));
+    var drugID = conDate + '--' + consumption.drug.id;
+
+    if (drugsPerDay.hasOwnProperty(drugID)) {
+      drugsPerDay[drugID] = drugsPerDay[drugID] + consumption.count;
+    } else {
+      drugsPerDay[drugID] = Number(consumption.count);
+    }
+  });
+
+  for (var k in drugsPerDay) {
+    if (drugsPerDay.hasOwnProperty(k)) {
+      var dateID = k.split('--');
+      var drugCount = drugsPerDay[k];
+      var drugData = allDrugs[dateID[1]];
+
+      $('#' + dateID[0]).append('<li>' + drugCount + ' ' + drugData.unit + ' ' + drugData.name + "</li>");
+    }
   }
 
   analyticsFinished += 1;
